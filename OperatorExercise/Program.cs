3e91fca7de0d0736986f664a10a17096ee878983
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-15 15:32:29 -0600

    Initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //--------------exercise 1------------
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+            var multiply = a * b;
+            var subtract = a - b;
+            var divide = a / b;
+            var modulus = a % b;
+
+            //2 more division variables
+
+            int quotent = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"Answers to the operations: {sum}; {multiply}; {subtract}; {divide}; {modulus};");
+            Console.WriteLine($"17 divided by 4 is {quotent} with a remainder of {remainder}.");
+
         }
     }
 }
